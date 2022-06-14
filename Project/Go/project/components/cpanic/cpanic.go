c11550df52e05b51627d1de997627fcda4fee251
528.ww@163.com <528.ww@163.com>
2022-06-14 10:22:37 +0800

    1,Add json-c project file.

--- Project/Go/project/components/cpanic/cpanic.go
+++ Project/Go/project/components/cpanic/cpanic.go
@@ -2,6 +2,7 @@ package cpanic
 
 /*
  * Copyright (c) 2022 chenjun
+ * Git: https://github.com/chenjunpc2008
  *
  * This is a free software; you can redistribute it and/or modify
  * it under the terms of the MIT license. See LICENSE for details.
