b186570de24b8921d0f033c155c734578657164c
528.ww@163.com <528.ww@163.com>
2022-07-13 11:08:33 +0800

    1,add kibana.

--- Deploy/deploy/log/deploy.sh
+++ Deploy/deploy/log/deploy.sh
@@ -29,7 +29,7 @@ exit_es_contains=`docker ps -a | grep -w "${PROJECT}-es"`
 if [ "$exit_es_contains" == "" ]; then
    docker run -d --name ${PROJECT}-es --network host -e "discovery.type=single-node" elasticsearch:8.3.2
    # 似乎elasticsearch有一个BUG，
-   # 不在初始配置下工作一段时间，生成certs下面的相关文件，
+   # 不在初始配置下工作一段时间，生成certs下面的相关文件后，
    # 才能转为elasticsearch.yml的xpack.security.enabled: false
    # 模式（无需用户认证，且为HTTP方式访问）工作。
    sleep 3m
@@ -46,3 +46,16 @@ chmod a+x *.sh
 ./deploy.sh -p ${PROJECT} -n ${PROJECT}-logstash -c "FALSE" -i ${IMAGE} -t ${TAG} -e "$EXTEND" -h ${HOMEPATH}
 cd ${SHHOMEPATH}
 rm -rf Dockerfile
+
+cp kibana_Dockerfile Dockerfile
+IMAGE="kibana_docker_image"
+TAG="latest"
+EXTEND="-v /dave/log:/dave/log -e ELASTICSEARCH_HOSTS=http://127.0.0.1:9200"
+cd ../../
+chmod a+x *.sh
+./deploy.sh -p ${PROJECT} -n ${PROJECT}-kibana -c "FALSE" -i ${IMAGE} -t ${TAG} -e "$EXTEND" -h ${HOMEPATH}
+cd ${SHHOMEPATH}
+rm -rf Dockerfile
+
+echo -e "Now \033[35mlog\033[0m is ready!"
+echo -e "Please browse the web: \033[35mhttp://[your IP address]:5601/app/management/data/index_management/indices\033[0m"
