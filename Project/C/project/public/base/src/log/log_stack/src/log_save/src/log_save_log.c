0dc4befc62c266fc2fc0a185e4374e8b8d2f4e85
528.ww@163.com <528.ww@163.com>
2022-08-09 14:45:33 +0800

    -

--- Project/C/project/public/base/src/log/log_stack/src/log_save/src/log_save_log.c
+++ Project/C/project/public/base/src/log/log_stack/src/log_save/src/log_save_log.c
@@ -109,6 +109,94 @@ _log_save_log_load_record(s8 *separator, s8 **record_ptr, ub *record_len, s8 *co
 	return content_index;
 }
 
+static inline ub
+_log_save_log_load_key_value(
+	s8 **key_start, s8 **key_end, s8 **value_start, s8 **value_end,
+	s8 *content_ptr, ub content_len)
+{
+	ub content_index;
+	s8 *head;
+
+	content_index = 0;
+
+key_value_find_start:
+	*key_start = *key_end = *value_start = *value_end = NULL;
+	head = NULL;
+
+	while(content_index < content_len)
+	{
+		if(content_ptr[content_index] != ' ')
+		{
+			head = &content_ptr[content_index];
+			break;
+		}
+		content_index ++;
+	}
+	if(head != NULL)
+	{
+		while(content_index < content_len)
+		{
+			if(content_ptr[content_index] == ' ')
+			{
+				if(*key_start == NULL)
+				{
+					content_index ++;
+					goto key_value_find_start; 
+				}
+				else
+				{
+					if(*key_end == NULL)
+					{
+						*key_end = &content_ptr[content_index];
+					}
+				}
+			}
+			else if(content_ptr[content_index] == ':')
+			{
+				*key_start = head;
+				if(*key_end == NULL)
+				{
+					*key_end = &content_ptr[content_index];
+				}
+				content_index ++;
+				break;
+			}
+			content_index ++;
+		}
+		if(*key_start != NULL)
+		{
+			while(content_index < content_len)
+			{
+				if(content_ptr[content_index] != ' ')
+				{
+					*value_start = &content_ptr[content_index];
+					break;
+				}
+				content_index ++;
+			}
+			if(*value_start != NULL)
+			{
+				while(content_index < content_len)
+				{
+					if((content_ptr[content_index] == ' ')
+						|| (content_ptr[content_index] == '\0'))
+					{
+						*value_end = &content_ptr[content_index];
+						break;
+					}
+					content_index ++;
+				}
+				if(content_index == content_len)
+				{
+					*value_end = &content_ptr[content_index];
+				}
+			}
+		}
+	}
+
+	return content_index;
+}
+
 static inline void
 _log_save_log_insert_fun_and_line(void *pJson, s8 *record_ptr, ub record_len)
 {
@@ -149,7 +237,7 @@ _log_save_log_insert_receive_date(void *pJson)
 }
 
 static inline ub
-_log_save_log_insert_content(void *pJson, s8 *content_ptr, ub content_len)
+_log_save_log_insert_public(void *pJson, s8 *content_ptr, ub content_len)
 {
 	s8 separator;
 	s8 *record_ptr;
@@ -209,6 +297,32 @@ _log_save_log_insert_content(void *pJson, s8 *content_ptr, ub content_len)
 	return content_index;
 }
 
+static inline void
+_log_save_log_insert_key_value(void *pJson, s8 *content_ptr, ub content_len)
+{
+	ub content_index;
+	s8 *key_start, *key_end, *value_start, *value_end;
+	ub safe_counter;
+	s8 backup_key_end;
+
+	content_index = safe_counter = 0;
+	while((content_index< content_len) && ((safe_counter ++) < content_len))
+	{
+		content_index += _log_save_log_load_key_value(
+			&key_start, &key_end, &value_start, &value_end,
+			&content_ptr[content_index], content_len-content_index);
+		if((key_start == NULL) || (key_end == NULL)
+			|| ((value_start == NULL) || (value_end == NULL)))
+			break;
+
+		backup_key_end = *key_end; *key_end = '\0';
+
+		dave_json_add_str_len(pJson, key_start, value_start, (ub)value_end-(ub)value_start);
+
+		*key_end = backup_key_end;
+	}
+}
+
 // =====================================================================
 
 void
@@ -224,7 +338,9 @@ log_save_log(sb file_id, TraceLevel level, s8 *content_ptr, ub content_len)
 
 	_log_save_log_insert_receive_date(pJson);
 	dave_json_add_str(pJson, "level", t_auto_TraceLevel_str(level));
-	process_len = _log_save_log_insert_content(pJson, content_ptr, content_len);
+	process_len = _log_save_log_insert_public(pJson, content_ptr, content_len);
+	_log_save_log_insert_key_value(pJson, &content_ptr[process_len], content_len-process_len);
+
 	if(process_len == 0)
 	{
 		dave_json_add_str_len(pJson, "log_body", content_ptr, content_len);
