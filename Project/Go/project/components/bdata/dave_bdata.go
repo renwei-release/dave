3c02e52c3b0b6795db5febc9b21426bdae76c93b
528.ww@163.com <528.ww@163.com>
2023-02-17 09:16:28 +0800

    -

---
+++ Project/Go/project/components/bdata/dave_bdata.go
@@ -0,0 +1,10 @@
+package bdata
+
+/*
+ * Copyright (c) 2023 Renwei
+ *
+ * This is a free software; you can redistribute it and/or modify
+ * it under the terms of the MIT license. See LICENSE for details.
+ */
+
+// =====================================================================
