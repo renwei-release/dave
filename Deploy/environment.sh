e17081a36d1404e314af720b4e0d43dc5d86392c
528.ww@163.com <528.ww@163.com>
2023-02-17 09:43:28 +0800

    -

--- Deploy/environment.sh
+++ Deploy/environment.sh
@@ -6,10 +6,17 @@
 # * it under the terms of the MIT license. See LICENSE for details.
 # */
 
+exit_yum=$(type yum)
+if [ "exit_yum" == ""]; then
+   INSTALL=yum
+else
+   INSTALL=apt
+fi
+
 exit_program=$(type docker)
 if [ "$exit_program" == "" ]; then
    echo environment.sh setup docker ...
-   sudo yum -y install docker
+   sudo ${INSTALL} -y install docker
    sudo systemctl start docker
    sudo systemctl enable docker
 fi
@@ -17,17 +24,17 @@ fi
 exit_program=$(type docker-compose)
 if [ "$exit_program" == "" ]; then
    echo environment.sh setup docker-compose ...
-   sudo yum -y install docker-compose
+   sudo ${INSTALL} -y install docker-compose
 fi
 
 exit_program=$(type bzip2)
 if [ "$exit_program" == "" ]; then
    echo environment.sh setup bzip2 ...
-   sudo yum -y install bzip2
+   sudo ${INSTALL} -y install bzip2
 fi
 
 exit_program=$(type wget)
 if [ "$exit_program" == "" ]; then
    echo environment.sh setup wget ...
-   sudo yum -y install wget
+   sudo ${INSTALL} -y install wget
 fi
